c91fc646f2cd9bfa4698401c2e5f033b759ebd2c
Shubhamjindal0801 <52204183+Shubhamjindal0801@users.noreply.github.com>
2023-04-24 21:15:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+const age = document.getElementById("age").value;
+const name1 = document.getElementById("name").value;
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", solve());
+function solve(){
+let promise = new Promise((resolve,reject) =>{
+	if(age>18){
+		resolve = true;
+	}
+	else{
+		reject = true;
+	}
+})
+
+promise.then((result)=>{
+  setTimeout(()=>{
+    alert(`Welcome, <${name1}>. You can vote.`)
+  },4000)
+})
+
+promise.catch((error)=>{
+  alert(`Oh sorry, <${name1}>. You aren't old enough.`)
+})
+}
